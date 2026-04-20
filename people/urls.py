e8755acd82b401d75410c5b409cf312847f096a9
PivotAlfa <backpacker0220@gmail.com>
2026-04-20 21:01:26 +0900

    updatelogin

--- people/urls.py
+++ people/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('profile/update/', views.profile_update, name='profile_update'),
     path('password-change/', auth_views.PasswordChangeView.as_view(template_name='people/password_change.html', 
     success_url=reverse_lazy('profile')),name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='people/password_change_done.html'), name='password_change_done'),
     ]
--- point/settings.py
+++ point/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third party
+    'widget_tweaks',
+
     'prime',
     'people',
     'analytics'
