38b95bd92d55ba6ee32a6051856d58931736ad3a
PivotAlfa <backpacker0220@gmail.com>
2026-04-28 19:34:06 +0900

    perf: gunicorn --preload, 3 workers, 30s timeout

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn point.wsgi --workers 2 --timeout 120 --bind 0.0.0.0:$PORT
+web: gunicorn point.wsgi --workers 3 --timeout 30 --preload --bind 0.0.0.0:$PORT
--- people/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PeopleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'people'
-
-    def ready(self):
-        import people.signals
